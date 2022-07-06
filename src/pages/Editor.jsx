3cc109f7471dc43c214d7fac5e42a2df95289ee4
PlanetNamekTech <sr23513@gmail.com>
2022-07-05 17:11:31 -0700

    added colorpicker and editor components

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,5 +1,19 @@
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from "@syncfusion/ej2-react-richtexteditor";
+
+import { EditorData } from "../data/dummy";
+import { Header } from "../components";
+
 const Editor = () => {
-    return ( <div>Poop</div> );
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent>
+        {/* EditorData is only dummy data, can start from fresh without */}
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
 }
- 
+
 export default Editor;
